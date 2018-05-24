9113979b95118dac70fc9fa3b5d37056bcdb8f77
faisal farooq <chaudhary.fsl@gmail.com>
2018-05-24 15:26:56 -0400

    routes updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  # path to homepage
   root to: 'pages#home'
-  
+  # path to about page
   get 'about', to: 'pages#about' 
-  
+  # path to ir dashboard
   get 'ir_dashboard', to: 'pages#ir_dashboard'
-  
+  # CRUD operations path for contacts model
   resources :contacts, only: :create
   get 'contact-us', to: 'contacts#new', as: 'new_contact'
 end
