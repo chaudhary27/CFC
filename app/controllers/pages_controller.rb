9d10556de17e32f6ee5236f5273a0ae105f731c0
faisal farooq <chaudhary.fsl@gmail.com>
2019-07-06 17:31:11 -0400

    remove contacts form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -17,7 +17,7 @@ class PagesController < ApplicationController
   end
 
   def virustotal
-    sha256  = '01ba4719c80b6fe911b091a7c05124b64eeece964e09c058ef8f9805daca546b'
+    sha256  = 'c35f705df9e475305c0984b05991d444450809c35dd1d96106bb8e7128b9082f'
     api_key = '860b6021a2bcca379c8f0d2370f2b5be287d39cb0538b9e2b4b7e7815cbdc8ce'
     vtreport = VirustotalAPI::FileReport.find(sha256, api_key)
     @vtreport = vtreport.exists?
